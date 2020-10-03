dc0706d8e01221d4cf902c49452e32afeb3e8123
amanviitb <amanviitb@gmail.com>
2020-10-04 01:38:12 +0530

    Make changes to complete request handlers

--- src/data/message.go
+++ src/data/message.go
@@ -1,22 +1,23 @@
 package data
 
-import (
-	"time"
-)
+import "errors"
+
+// ErrMessageNotFound is the error that is returned when there is not matching message
+var ErrMessageNotFound = errors.New("no message found with the given ID")
 
 // Message is the structure for a message
 type Message struct {
-	ID     string    `json:"id"`
-	Text   string    `json:"text"`
-	Sender string    `json:"sender"`
-	Time   time.Time `json:"time,omitempty"`
+	ID     string `json:"id"`
+	Text   string `json:"text"`
+	Sender string `json:"sender"`
+	Time   string `json:"-"`
 }
 
 // Messages is the collection of all messages
 type Messages []*Message
 
 var messages Messages = Messages{
-	&Message{ID: "123", Text: "Olo", Sender: "Su"},
+	&Message{ID: "123", Text: "Olo", Sender: "Stu"},
 	&Message{ID: "1122", Text: "Hello", Sender: "Sam"},
 }
 
@@ -25,10 +26,42 @@ func GetMessages() Messages {
 	return messages
 }
 
+// AddMessage adds the message to the list of messages
+func AddMessage(msg *Message) {
+	messages = append(messages, msg)
+}
+
+// GetMessageByID returns a message for a given messageID
+func GetMessageByID(messageID string) (*Message, error) {
+	for i := range messages {
+		if messages[i].ID == messageID {
+			return messages[i], nil
+		}
+	}
+	return nil, ErrMessageNotFound
+}
+
+// DeleteMessageWithID deletes a message with the given ID
+func DeleteMessageWithID(messageID string) error {
+	var indexToDelete = -1
+	for i := range messages {
+		if messageID == messages[i].ID {
+			indexToDelete = i
+			break
+		}
+	}
+	// no message with the given ID was found
+	if indexToDelete == -1 {
+		return ErrMessageNotFound
+	}
+	messages = append(messages[:indexToDelete], messages[indexToDelete+1])
+	return nil
+}
+
 // improve logging
 // add versioning to the APIs like /api/v1
 // add some validation to the fields of a message
-// dockerfile and creating image
+// dockerfile and creating image instructions
 // test cases
-// palindromic string
+// palindromic string - done
 // observed”(Monitoring/Traceability/metrics)
--- src/server/handlers.go
+++ src/server/handlers.go
@@ -9,6 +9,7 @@ import (
 )
 
 // handleGetMessages is the handler for GET request to fetch all messages
+// GET /messages
 func (s *server) handleGetMessages() http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		s.logger.Info(req.Method, "Get All Messages")
@@ -20,7 +21,9 @@ func (s *server) handleGetMessages() http.HandlerFunc {
 	}
 }
 
+// POST /messages
 func (s *server) handlePostMessage() http.HandlerFunc {
+	// add logic to generate an ID, create a message object and assign the ID to it
 	return func(rw http.ResponseWriter, req *http.Request) {
 		message := new(data.Message)
 		err := data.FromJSON(message, req.Body)
@@ -29,23 +32,43 @@ func (s *server) handlePostMessage() http.HandlerFunc {
 			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		data.AddMessage(message)
 		rw.WriteHeader(http.StatusCreated)
 	}
 }
 
+// GET /messages/{id}
 func (s *server) handleGetSingleMessage() http.HandlerFunc {
+	// a separate response for message
+	type response struct {
+		MessageText  string `json:"messageText"`
+		IsPalindrome bool   `json:"isPalindrome"`
+	}
+
 	return func(rw http.ResponseWriter, req *http.Request) {
 		// parse the request to fetch the id from the URI
 		pathVars := mux.Vars(req)
-		messageID, ok := pathVars["id"]
-		if !ok {
-			http.Error(rw, "Unknown query parameter passed", http.StatusBadRequest)
+		messageID := pathVars["id"]
+
+		// assuming each message gets its unique ID
+		err := data.DeleteMessageWithID(messageID)
+
+		switch err {
+		case nil:
+		case data.ErrMessageNotFound:
+			s.logger.Error("Unable to fetch message", "error: ", err)
+			http.Error(rw, "No message found with the given ID", http.StatusNotFound)
+			return
+		default:
+			s.logger.Error("Unable to fetch message", "error: ", err)
+			http.Error(rw, "", http.StatusInternalServerError)
 			return
 		}
-		s.logger.Info(messageID)
+		rw.WriteHeader(http.StatusNoContent)
 	}
 }
 
+// DELETE /messages/{id}
 func (s *server) handleDeleteMessage() http.HandlerFunc {
 	// check to see if the id exists, if not send http.StatusNotFound
 	// else delete the resource and if error occurs return http.StatusInternalServerError
@@ -55,19 +78,25 @@ func (s *server) handleDeleteMessage() http.HandlerFunc {
 	}
 }
 
+// checkIfPalindrome checks to see if the given string is a palindrome
 func checkIfPalindrome(s string) bool {
+	// an empty string or a string with a single character is always a palindrome
 	if len(s) == 0 || len(s) == 1 {
 		return true
 	}
+	// two indices to keep track of each character from left and right
 	var left, right = 0, len(s) - 1
 	for left <= right {
-		if strings.ToLower(string(s[right])) < string('a') || strings.ToLower(string(s[right])) > string('z') {
+		if strings.ToLower(string(s[right])) < string('a') ||
+			strings.ToLower(string(s[right])) > string('z') { // jump over the non-alphabet character from right
 			right--
-		} else if strings.ToLower(string(s[left])) < string('a') || strings.ToLower(string(s[left])) > string('z') {
+		} else if strings.ToLower(string(s[left])) < string('a') ||
+			strings.ToLower(string(s[left])) > string('z') { // jump over the non-alphabet character from left
 			left++
-		} else if strings.ToLower(string(s[right])) != strings.ToLower(string(s[left])) {
+		} else if strings.ToLower(string(s[right])) !=
+			strings.ToLower(string(s[left])) { // characters at both ends don't match
 			return false
-		} else {
+		} else { // characters at both ends match
 			left++
 			right--
 		}
